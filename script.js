f7af19996ae2390db3deea5fd5278b46302ebf3c
USHA RANI GANTA <GANTAUSHARANI26@GMAIL.COM>
2025-05-23 17:58:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function showSurprise() {
+    document.getElementById('surprise').classList.remove('hidden');
+    document.getElementById('message').innerText = "Here's your birthday surprise, ghani";
+  }
+  
+  function showVideo() {
+    document.getElementById('videoContainer').classList.remove('hidden');
+  }
